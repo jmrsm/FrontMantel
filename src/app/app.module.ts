897a86a8768d10ffcf9d66f932bcba6026876b5e
waldo <jmrsm01@gmail.com>
2017-09-29 19:24:07 -0300

    Agrego Vista general Contenidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+
+const SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  keyboardControl: true
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContenidoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule.forRoot(SWIPER_CONFIG)
   ],
   providers: [],
   bootstrap: [AppComponent]
