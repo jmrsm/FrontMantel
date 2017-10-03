c20b6619e2fbb645fd78fdfa7548888ab7e3e7a3
waldo <jmrsm01@gmail.com>
2017-10-03 03:14:26 -0300

    Agregado de Inicio de sesion vista de perfil y utilizacin de router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,21 @@ import { ContenidoComponent } from './contenido/contenido.component';
 import { ContenidoListComponent } from './contenido/contenido-list/contenido-list.component';
 import { ContentComponent } from './contenido/contenido-list/content.component';
 import { ContentDetailComponent } from './contenido/content-detail/content-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import {FormsModule} from '@angular/forms';
+
+
+const  appRoutes: Routes = [
+  {path:'',redirectTo:'/contenido',pathMatch:'full'},
+  {path:'contenido',component:ContenidoComponent},
+  {path:'login',component:LoginComponent},
+  {path:'perfil',component:PerfilComponent},
+  {path:'admin',component:AdminComponent}
+];
+
 
 const SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
@@ -25,10 +40,15 @@ const SWIPER_CONFIG: SwiperConfigInterface = {
     ContenidoComponent,
     ContenidoListComponent,
     ContentComponent,
-    ContentDetailComponent
+    ContentDetailComponent,
+    PerfilComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
+    FormsModule,
     SwiperModule.forRoot(SWIPER_CONFIG)
   ],
   providers: [ContentService],
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {NgForm} from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router:Router) { 
+  }
+
+  ngOnInit() {
+  }
+  login(form: NgForm){
+    
+    console.log(form.value);
+    //var user = e.target.element[0].value;
+    //var pass = e.target.element[1].value;
+    //console.log(user);
+    if(form.value.email=='admin@admin' && form.value.password=='admin'){
+      this.router.navigate(['admin']);
+    }else{
+      this.router.navigate(['contenido']);
+    }
+
+    return false;
+  }
+
+}
