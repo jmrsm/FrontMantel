ba14bb3f0fe08f3556a90ffd93a04db929390317
maria.medina@fing.edu.uy <maria.medina@fing.edu.uy>
2017-10-14 18:48:51 -0300

    Alta de admin no està persistiendo todavìa da un error 415

--- src/app/altaadmin/altaadmin.component.ts
+++ src/app/altaadmin/altaadmin.component.ts
@@ -1,22 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {ActivatedRoute, Router } from '@angular/router';
+import {AdminService} from '../services/admin.service';
+
 
 @Component({
   selector: 'app-altaadmin',
   templateUrl: './altaadmin.component.html',
-  styleUrls: ['./altaadmin.component.css']
+  styleUrls: ['./altaadmin.component.css'],
+  providers: [AdminService],
 })
 export class AltaadminComponent implements OnInit {
+  options: string[] = [];
+  status: any = '';
+  exito: string = '';
+  errorMessage: string = '';
+  error: string = '';
+  isLoading: boolean = true;
+  msj: string = '';
+  link: string = '';
+  constructor(private Adminservice: AdminService ,private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+   ngOnInit() {
   }
   Altaadmin(form: NgForm){
+    var email=form.value.email;
+    var password=form.value.password;
+    var nombre=form.value.nombre;
+    var apellido=form.value.apellido;
+    //console.log(form.value.email);
+    var body='email='+email+'&password='+password+'&nombre='+nombre+'&apellido='+apellido+'&proveedorContenidoId=2p';
+    
     
     console.log(form.value);
-    
-    return false;
-  }
+   
+   this.Adminservice.addAdmin(body).subscribe(p => {
+      //console.log("dentro de if por 200");
+      this.exito = 'Administrador creado con éxito';
+      this.router.navigate(['/login']);
 
-}
+    },e => this.error = e, () => this.isLoading = false);
+    /*this.Adminservice.addAdmin(body).subscribe(p => {
+
+        this.status = JSON.parse(p['_body']);
+
+          if (this.status = '200'){
+
+            console.log("dentro de if por 200");
+            this.exito = 'Administrador creado con éxito';
+            this.router.navigate(['/login']);
+
+          }
+
+      },e => this.error = e, () => this.isLoading = false);*/
+    return false;
+   }
+  }
+   
+    
+ 
---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
+
+@Injectable()
+export class AdminService {
+  private url:string= 'http://localhost:8080/';
+  constructor(private http:Http) { }
+
+  addAdmin(body:String): Observable<any>{
+    console.log(this.url+'api/superAdmin/altaAdmin?'+body);
+    
+    return  this.http.post(this.url+'api/superAdmin/altaAdmin', body, {headers: this.getHeaders()});
+    
+  }
+
+  
+  private getHeaders(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    console.log(headers.toJSON);
+    return headers;
+
+  }
+  
+}
