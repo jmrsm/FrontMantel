3a12a9d3faf8c7e7c8c97641fe8d66e78cbbcdd9
maria.medina@fing.edu.uy <maria.medina@fing.edu.uy>
2017-10-16 19:27:32 -0300

    Servicio que faltaba en altaEmpresa

---
+++ src/app/models/empresa.ts
@@ -0,0 +1,5 @@
+export class Empresa{
+    constructor( public nombre:String){
+
+    }
+}
---
+++ src/app/services/empresa.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/Rx';
+import {Empresa} from '../models/empresa';
+
+@Injectable()
+export class EmpresaService {
+
+  private url:string= 'http://localhost:8080/';
+  constructor(private http:Http) { }
+
+  addEmpresa(body:String): Observable<any>{
+    console.log(this.url+'api/public/proveedorContenido/?'+body);
+    //return this.http.get(this.url+'proveedorContenido/?'+body);
+    return  this.http.post(this.url+'/api/public/proveedorContenido', body, {headers: this.getHeaders()});
+    
+  }
+  login(body:String): Observable<any>{
+    console.log(this.url+'api/public/login?'+body);
+    return this.http.get(this.url+'api/public/login/'+body);
+  }
+ 
+  private getHeaders(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return headers;
+  }
+}
