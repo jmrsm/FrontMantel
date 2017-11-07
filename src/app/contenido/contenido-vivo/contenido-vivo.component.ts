8e4f5ef20a637c2a79196f1b2cded84bc87b8346
javie <javie@DESKTOP-82SF0BI>
2017-11-06 23:52:56 -0300

    Subo reproduccion de contenido en vivo con chat integrado. Borro datos que subi por error en el commit anterior.

--- src/app/contenido/contenido-vivo/contenido-vivo.component.ts
+++ src/app/contenido/contenido-vivo/contenido-vivo.component.ts
@@ -3,8 +3,12 @@ import {NgForm} from '@angular/forms';
 import {VgAPI, VgFullscreenAPI} from 'videogular2/core';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 import {SimpleTimer} from 'ng2-simple-timer';
-import { Content } from '../../models/content';
-import { ContentService } from '../../services/content.service';
+import {Content} from '../../models/content';
+import {ContentService} from '../../services/content.service';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import { AngularFireDatabase,AngularFireObject,AngularFireList } from 'angularfire2/database';
+
 @Component({
   selector: 'app-repro-vivo',
   templateUrl: './contenido-vivo.component.html',
@@ -12,6 +16,19 @@ import { ContentService } from '../../services/content.service';
   providers: [ContentService]
 })
 export class ContenidoVivoComponent implements OnInit {
+  //------------------------Contador------------------------//
+  private future: Date;
+  private futureString: string;
+  private diff: number;
+  private $counter: Observable<number>;
+  private subscription: Subscription;
+  private message: string;
+  //-------------------------------------------------------//
+  
+  //--------------------------Chat--------------------------//
+  itemsRef: AngularFireList<any>;  
+  item: Observable<any[]>;
+  //--------------------------------------------------------//
   sources: Array<Object>;
   api: VgAPI;
   fsAPI: VgFullscreenAPI;
@@ -22,32 +39,53 @@ export class ContenidoVivoComponent implements OnInit {
   public idVideo;
   public idUsuario;
   public timerId: string;
-  fechaComienzo: string = '11/6/2017 00:45:00';
+  fechaComienzo: string = '11/6/2017 23:43:00';
   cFecheDate: Date;
   fechaServidor: string;
   sFechaDate: Date;
-  
+
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
     private st: SimpleTimer,
-    private contentservice: ContentService
-  ) {}
+    private contentservice: ContentService,
+    private db: AngularFireDatabase
+  ) {
+    this.itemsRef = db.list('chat1');
+    this.item = db.list('chat1').valueChanges();
+  
+  }
 
   ngOnInit() {
-      this._route.params.forEach((params: Params) => {
+    this._route.params.forEach((params: Params) => {
       this.srcVideo = localStorage.getItem('videoSrc');
       this.startTimeVideo = localStorage.getItem('videoTime');
       this.idVideo = localStorage.getItem('videoId');
 
-      this.cargarFechaServidor();
       this.cFecheDate = new Date(this.fechaComienzo);
-        
+      this.cargarFechaServidor();
       this.st.newTimer('1sec', 1);
-        
+
     });
+
+    this.sources = [
+      {
+        src: this.srcVideo,
+        type: "video/mp4"
+      },
+
+      {
+        src: this.srcVideo,
+        type: "video/ogg"
+      },
+
+      {
+        src: this.srcVideo,
+        type: "video/webm"
+      }
+    ];
   }
-  
+
   // Se carga la fecha del servidor
   cargarFechaServidor() {
     this.contentservice.getServerDate().subscribe(
@@ -59,56 +97,89 @@ export class ContenidoVivoComponent implements OnInit {
       error => {
         console.log(<any>error);
       }
-    ); 
+    );
+  }
+
+  // Se comienza la reproduccion  dependiendo de la fecha de comienzo del contenido y la fecha actual en el servidor
+
+  comenzarReproduccion() {
+
+    if (this.sFechaDate < this.cFecheDate) {
+      this.iniciarEspera();
+    }
+    else {
+      this.iniciarReproduccion();
+    }
+  }
+
+  // Si la fecha del contenido es posterior a la fecha del servidor se inicia un contador paara el inicio
+  iniciarEspera() {
+    this.api.getDefaultMedia().canPlay = false;
+    this.api.getDefaultMedia().isLive = true;
+//    this.futureString = 'November 6, 2017 21:40:00';
+    
+    
+//        this.future = new Date(this.futureString);
+        this.future = new Date(this.fechaComienzo);
+        this.$counter = Observable.interval(1000).map((x) => {
+            this.diff = Math.floor((this.future.getTime() - new Date().getTime()) / 1000);
+            return x;
+        });
+
+        this.subscription = this.$counter.subscribe((x) => this.message = this.dhms(this.diff));
+  }
+
+  // Si la fecha del contenido es igual o anterior a la fecha del servidor se inicia la reproduccion
+  // Mejora a hacer, teniendo la duracion del contenido se podria simular un fin de transmision
+  iniciarReproduccion() {
+    let dserv = this.sFechaDate.getTime() / 1000;
+    let dcont = this.cFecheDate.getTime() / 1000;
+
+    let res = dserv - dcont;
+    this.currentTimeVideo = res;
+    this.play();
+
+  }
+
+  onPlayerReady(api: VgAPI) {
+    this.api = api;
+//    this.track = this.api.textTracks[0];
+    this.fsAPI = this.api.fsAPI;
+    this.nativeFs = this.fsAPI.nativeFullscreen;
+  }
+
+  //Comienza la reproduccion
+  play() {
+    this.api.getDefaultMedia().currentTime = this.currentTimeVideo;
+    this.api.getDefaultMedia().play();
+  }
+
+  dhms(t) {
+    var d, h, m, s;
+    d = Math.floor(t / 86400);
+    t -= d * 86400;
+    h = Math.floor(t / 3600) % 24;
+    t -= h * 3600;
+    m = Math.floor(t / 60) % 60;
+    t -= m * 60;
+    s = t % 60;
+    if (d === 0 && h === 0 && m === 0 && s === 0)   {
+      this.iniciarReproduccion();
+    }
+    return [
+      d + ' Dias',
+      h + ' Horas',
+      m + ' Minutos',
+      s + ' Segundos'
+    ].join(' ');
   }
   
-  // Se comienza la reproduccion  dependiendo de la fecha de comienzo del contenido y la fecha actual en el servidor
   
-    comenzarReproduccion() {
-      console.log('serv: '+this.sFechaDate+' cont: '+this.cFecheDate);
-      
-      if (this.sFechaDate < this.cFecheDate) {
-          this.iniciarEspera();
-      }
-      else {
-          this.iniciarReproduccion();
-      }
-    }
-  
-    // Si la fecha del contenido es posterior a la fecha del servidor se inicia un contador paara el inicio
-    iniciarEspera() {
-        
-    }
-  
-    // Si la fecha del contenido es igual o anterior a la fecha del servidor se inicia la reproduccion
-    // Mejora a hacer, teniendo la duracion del contenido se podria simular un fin de transmision
-    iniciarReproduccion() {
-      let dserv = this.sFechaDate.getTime() / 1000;
-      let dcont = this.cFecheDate.getTime() / 1000;
-      
-      let res = dserv - dcont; 
-      this.currentTimeVideo = res;
-      
-      this.play(this.api);
-      
-    }
-  
-     onPlayerReady(api: VgAPI) {
-        this.api = api;
-        this.fsAPI = this.api.fsAPI;
-        this.nativeFs = this.fsAPI.nativeFullscreen;
-        
-        this.api.getDefaultMedia().currentTime = +this.startTimeVideo;
-        this.api.getDefaultMedia().play();
-    }
-  
-    play(api: VgAPI) {
-        this.api = api;
-        
-        this.api.getDefaultMedia().currentTime = +this.startTimeVideo;
-        this.api.getDefaultMedia().play();
-    }
-  
-  
-  
+   enviar(form: NgForm){
+    var mensaje=form.value.mensaje;
+    var nombre=form.value.nombre;
+    console.log(mensaje+' '+nombre);
+    this.itemsRef.push({name: nombre,message:mensaje});
+  }
+
 }
--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -36,12 +36,6 @@ export class ContentService {
     return  this.http.get(this.url+'api/admin/getID'+body, {headers: this.getHeaders()});
   }
   addContenido(body:string){
-    //https://firebasestorage.googleapis.com/v0/b/tsi2-9f457.appspot.com/o/videos%2FMarvels_The_Defenders__Official_Trailer_HD__Netflix(youtube.com).mp4?alt=media%26token=12319da7-b1c8-4823-8108-3e6383e08347
-    //https://firebasestorage.googleapis.com/v0/b/tsi2-9f457.appspot.com/o/videos%2FMarvels_The_Defenders__Official_Trailer_HD__Netflix(youtube.com).mp4?alt=media&token=12319da7-b1c8-4823-8108-3e6383e08347
-    //https://firebasestorage.googleapis.com/v0/b/tsi2-9f457.appspot.com/o/videos%2FStar_Wars_The_Last_Jedi_Trailer_Official(youtube.com).mp4?alt=media&token=55148834-60da-4710-a0de-9ff2d27c9263 
-    //https://firebasestorage.googleapis.com/v0/b/tsi2-9f457.appspot.com/o/videos/JUSTICE_LEAGUE_Final_Trailer_Espaol_Extendido_2017(youtube.com).mp4?alt=media&token=55cc00f3-e3f3-4478-b863-53a370cf8d08
-    //https://firebasestorage.googleapis.com/v0/b/tsi2-9f457.appspot.com/o/videos%2FJUSTICE_LEAGUE_Final_Trailer_Espaol_Extendido_2017(youtube.com).mp4?alt=media&token=55cc00f3-e3f3-4478-b863-53a370cf8d08   
-    //http://localhost:8080//api/admin/contenidoOmdb?proveedorContenidoId=1&omdbId=tt2488496&path=https://firebasestorage.googleapis.com/v0/b/tsi2-9f457.appspot.com/o/videos%2FStar_Wars_The_Last_Jedi_Trailer_Official(youtube.com).mp4?alt=media&token=55148834-60da-4710-a0de-9ff2d27c9263&esSerie=false&esDestacado=true
     console.log(this.url+'/api/admin/contenidoOmdb?'+body);
     return this.http.post(this.url+'api/admin/contenidoOmdb',body, {headers: this.getHeaders()});
   }
