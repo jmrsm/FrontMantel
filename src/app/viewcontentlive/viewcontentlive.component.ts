eae1f1227f6d9cc06badae5391df44720e5160f8
waldo <jmrsm01@gmail.com>
2017-11-13 18:11:45 -0300

    Integracion de chat

--- src/app/viewcontentlive/viewcontentlive.component.ts
+++ src/app/viewcontentlive/viewcontentlive.component.ts
@@ -2,22 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase,AngularFireObject,AngularFireList } from 'angularfire2/database';
 
 import { Observable } from 'rxjs/Observable';
+import {UserService} from '../services/user.service';
 import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-viewcontentlive',
   templateUrl: './viewcontentlive.component.html',
   styleUrls: ['./viewcontentlive.component.css'],
   providers:[
-    AngularFireDatabase
+    AngularFireDatabase,
+    UserService
   ]
 })
 export class ViewcontentliveComponent implements OnInit {
   itemsRef: AngularFireList<any>;  
   item: Observable<any[]>;
+  nick:string;
+  todo:string='Todos';
+  destinatario:string;
   constructor(private db: AngularFireDatabase) { 
-    this.itemsRef = db.list('chat1');
-    this.item = db.list('chat1').valueChanges();
-
+    this.itemsRef = db.list('prueba12');
+    this.item = db.list('prueba12').valueChanges();
+    console.log(this.item);
+    this.nick='Admin';//localStorage.getItem('email');
   }
 
   ngOnInit() {
@@ -25,7 +31,24 @@ export class ViewcontentliveComponent implements OnInit {
   enviar(form: NgForm){
     var mensaje=form.value.mensaje;
     var nombre=form.value.nombre;
-    console.log(mensaje+' '+nombre);
-    this.itemsRef.push({name: nombre,message:mensaje});
+    var d=form.value.destinatario;
+    var destinatario= d.split(";");
+    if(destinatario==''){
+      this.itemsRef.push({name: this.nick,message:mensaje,addressee: this.todo});    
+    }else{
+      for(let c of destinatario){
+        
+          this.itemsRef.push({name: this.nick,message:mensaje,addressee: c});  
+      }  
+    }
+    /*
+    for(let c of destinatario){
+      if(c){
+        this.itemsRef.push({name: this.nick,message:mensaje,addressee: this.todo});    
+      }else{
+        this.itemsRef.push({name: this.nick,message:mensaje,addressee: c});  
+      }
+      
+    }*/
   }
 }
