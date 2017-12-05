3f5b3bb5cbf79260c796c56e730732b1b9e41745
javier <javierp55@github.com>
2017-12-05 17:12:03 -0300

    cambios

---
+++ src/app/reportes/reportes-superadmin/reportes-superadmin.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../services/user.service';
+
+@Component({
+  selector: 'app-reportes-superadmin',
+  templateUrl: './reportes-superadmin.component.html',
+  styleUrls: ['./reportes-superadmin.component.css']
+})
+export class ReportesSuperadminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
